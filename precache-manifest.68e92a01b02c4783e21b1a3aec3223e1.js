776c960357b40c1ed8eaaefb61f9b402033d22c8
Kasi <57307447+jhlee225@users.noreply.github.com>
2020-05-11 20:12:16 +0900

    Upload Build File

---
+++ precache-manifest.68e92a01b02c4783e21b1a3aec3223e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d06d6c8e03505c9ec51ccc37e027e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20455ebd0a6fae435c54",
+    "url": "/static/css/main.3ec0bd72.chunk.css"
+  },
+  {
+    "revision": "1c7971d2569abc537588",
+    "url": "/static/js/2.23c7084a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.23c7084a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20455ebd0a6fae435c54",
+    "url": "/static/js/main.9f4d138b.chunk.js"
+  },
+  {
+    "revision": "cdd5a04d917c8bc0b8e7",
+    "url": "/static/js/runtime-main.377bcdda.js"
+  },
+  {
+    "revision": "477be021418e16726b51b9b157eb405d",
+    "url": "/static/media/book.477be021.jpg"
+  }
+]);
